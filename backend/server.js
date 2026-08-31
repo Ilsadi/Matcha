c4b25e63250c5a60f4ed755623f4b5b7502c6c07
Akim <akim.hamini11@gmail.com>
2026-08-31 09:45:42 +0200

    Ajout de la route GET "/users"

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,24 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 
+let users = [
+  { id: 1,
+    name: 'Alban',
+    age: 25,
+    city: 'Annecy' 
+  },
+  { id: 2,
+    name: 'Bob',
+    age: 30,
+    city: 'Bordeaux'
+  },
+  { id: 3,
+    name: 'Clemence',
+    age: 35,
+    city: 'Caen'
+  }
+];
+
 const confirmServerIsRunning = (req, res) => {
   res.status(200).json({
     status: 'ok',
@@ -10,8 +28,18 @@ const confirmServerIsRunning = (req, res) => {
   });
 }
 
+const getUsers = (req, res) => {
+  res.status(200).json(users)({
+    status: "success",
+    data: users
+  })
+};
+
 app.get('/', confirmServerIsRunning);
+
+app.get('/users', getUsers);
 
 app.listen(PORT, () => {
   console.log(`Serveur backend lancé sur http://localhost:${PORT}`);
+  console.log('Utilisateurs temporaires en mémoire :', users);
 });
